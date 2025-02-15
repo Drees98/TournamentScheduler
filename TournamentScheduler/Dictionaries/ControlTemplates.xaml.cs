87f5ad6e3b9b49073a558277929e2c5d3107b7ee
Drees98 <davidrees191@gmail.com>
2025-02-14 16:31:59 -0800

    Add project files.

---
+++ TournamentScheduler/Dictionaries/ControlTemplates.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TournamentScheduler.Dictionaries
+{
+    /// <summary>
+    /// Interaction logic for ControlTemplates.xaml
+    /// </summary>
+    public partial class ControlTemplates : ResourceDictionary
+    {
+        private void OnTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var exp = textBox.GetBindingExpression(TextBox.TextProperty);
+            if (exp == null) return;
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                if (textBox.Tag is ICommand command && command.CanExecute(textBox.Text))
+                {
+                    command.Execute(textBox.Text);
+                }
+                else
+                {
+                    exp.UpdateSource();
+                }
+                Keyboard.ClearFocus();
+                e.Handled = true;
+            }
+            else if (e.Key == System.Windows.Input.Key.Escape)
+            {
+                exp.UpdateTarget();
+                Keyboard.ClearFocus();
+            }
+        }
+        private void OnTextBoxRename_KeyDown(object sender, KeyEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var exp = textBox.GetBindingExpression(TextBox.TextProperty);
+            if (exp == null) return;
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                if (textBox.Tag is ICommand command && command.CanExecute(textBox.Text))
+                {
+                    command.Execute(textBox.Text);
+                }
+                else
+                {
+                    exp.UpdateSource();
+                }
+                textBox.Visibility = Visibility.Collapsed;
+                e.Handled = true;
+            }
+            else if (e.Key == System.Windows.Input.Key.Escape)
+            {
+                exp.UpdateTarget();
+                textBox.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void OnTextBoxRename_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            if (!textBox.IsVisible) return;
+            var exp = textBox.GetBindingExpression(TextBox.TextProperty);
+            if (exp != null)
+            {
+                exp.UpdateTarget();
+                textBox.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void OnClose_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.Close();
+        }
+
+        private void OnMaximiseRestore_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.WindowState = (window.WindowState == WindowState.Maximized) ?
+                WindowState.Normal : WindowState.Maximized;
+        }
+
+        private void OnMinimize_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.WindowState = WindowState.Minimized;
+        }
+    }
+}
---
+++ TournamentScheduler/SchedulerControl/Teams.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentScheduler.SchedulerControl
+{
+    class Teams
+    {
+		private static ObservableCollection<Team> _teamsList = new ObservableCollection<Team>{new Team("Team1"), new Team("Team2") };
+		public ReadOnlyObservableCollection<Team> TeamsList { get; private set; }
+
+		public Teams()
+		{
+			TeamsList = new ReadOnlyObservableCollection<Team>(_teamsList);
+		}
+	}
+
+	class Team : ItemBase
+	{
+		private string _name;
+		public string Name { 
+			get => _name;
+			set
+			{
+				if (value != _name) 
+				{
+					_name = value;
+					OnPropertyChanged(nameof(Name));
+				}
+			} 
+		}
+
+		public Team(string name)
+		{
+			_name = name;
+		}
+	}
+}
